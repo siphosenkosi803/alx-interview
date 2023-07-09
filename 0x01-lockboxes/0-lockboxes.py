22cf390f16c11ccd82176daa1d09c6eec6831e50
siphosenkosi803 <sibusisoradebe043@yahoo.com>
2023-07-09 05:02:34 -0700

    added complete lockboxes script and test script

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Unlocks lists of lists using canUnlockAll function
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    A method that determines if all the boxes can be opened
+    """
+
+    visited_boxes = [0]
+
+    for box in visited_boxes:
+        keys = boxes[box]
+        for key in keys:
+            if key not in visited_boxes and key < len(boxes):
+                visited_boxes.append(key)
+
+    return len(visited_boxes) == len(boxes)
+
