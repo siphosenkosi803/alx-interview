8ca26146b33922c638067707e96494d329797411
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-07-22 14:08:24 +0200

    PEP 8 style fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,54 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
+""" Script that reads stdin line by line and computes metrics """
 
 import sys
 
-def printsts(dic, size):
+def print_stats(statistics, total_size):
     """ Prints information """
-    # Print the total file size
-    print("File size: {:d}".format(size))
-
-    # Print the count of each status code in ascending order
-    for code, count in sorted(dic.items()):
+    print(f"File size: {total_size}")
+    for code, count in sorted(statistics.items()):
         if count != 0:
             print(f"{code}: {count}")
 
 # Initialize a dictionary to store the count of each status code
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
 
 # Initialize counters for total number of lines processed and total file size
-count = 0
-size = 0
+line_count = 0
+total_size = 0
 
 try:
     # Read each line from the standard input
     for line in sys.stdin:
         # Print statistics after every 10 lines
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        if line_count != 0 and line_count % 10 == 0:
+            print_stats(status_codes, total_size)
 
         # Split the line into a list of strings
-        stlist = line.split()
-        count += 1
+        line_list = line.split()
+        line_count += 1
 
         try:
             # Extract the file size from the line and add it to the total file size
-            size += int(stlist[-1])
-        except:
+            total_size += int(line_list[-1])
+        except (ValueError, IndexError):
             # If there is an exception (e.g., invalid file size), skip the line
-            pass
+            continue
 
         try:
             # Extract the status code from the line and increment its count in the dictionary
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
+            status_code = line_list[-2]
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except (IndexError, TypeError):
             # If there is an exception (e.g., invalid status code), skip the line
-            pass
+            continue
 
     # Print the final statistics after processing all lines
-    printsts(sts, size)
+    print_stats(status_codes, total_size)
 
 except KeyboardInterrupt:
     # If the user interrupts the script (CTRL + C), print the current statistics
-    printsts(sts, size)
+    print_stats(status_codes, total_size)
     raise
-
