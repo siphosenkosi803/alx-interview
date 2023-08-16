51e416e431c456f0bcda93533d71923a48b66843
siphosenkosi803 <sibusisoradebe943@yahoo.com>
2023-08-16 07:28:26 -0700

    2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-
-""" Given an n x n 2D matrix, rotate it 90 degrees clockwise. """
+'''Given an n x n 2D matrix, rotate it 90 degrees clockwise.'''
 
 
 def rotate_2d_matrix(matrix):
-    """ Prototype: def rotate_2d_matrix(matrix):
-    Do not return anything. The matrix must be edited in-place
-    You can assume the matrix will have 2 dimensions and will not be empty.
-    """
-    n = len(matrix)
-    begin = 0
-    end = n - 1
+    '''rotates a 2d matrix 90°'''
+    lhs, rhs = 0, len(matrix) - 1
 
-    while begin < end:
-        offset = 0
-        for i in range(begin, end):
-            exo = matrix[begin][i]
-            matrix[begin][i] = matrix[end - offset][begin]
-            matrix[end - offset][begin] = matrix[end][end - offset]
-            matrix[end][end - offset] = matrix[i][end]
-            matrix[i][end] = exo
-            offset += 1
-        begin += 1
-        end -= 1
-
-# Sample matrix
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
-
-rotate_2d_matrix(matrix)
-
-# Print the rotated matrix with proper formatting
-for row in matrix:
-    print(row)
+    while lhs < rhs:
+        for i in range(rhs - lhs):
+            up, down = lhs, rhs
+            # save uplhs  value
+            upLeft = matrix[up][lhs + i]
+            # move down lhs to up lhs
+            matrix[up][lhs + i] = matrix[down - i][lhs]
+            # move down rhs to down lhs
+            matrix[down - i][lhs] = matrix[down][rhs - i]
+            # move up rhs to down rhs
+            matrix[down][rhs - i] = matrix[up + i][rhs]
+            # move up lhs to up rhs
+            matrix[up + i][rhs] = upLeft
+        rhs -= 1
+        lhs += 1
 
